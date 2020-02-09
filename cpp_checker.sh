ef6aa568ddadbcfa2143c04305f2797c2619fd51
Hassan Pouralijan <pouralijan@gmail.com>
2020-02-09 23:13:28 +0330

    Update cpp_checker.sh

--- cpp_checker.sh
+++ cpp_checker.sh
@@ -5,7 +5,6 @@ cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.t
 PAYLOAD_CLANG=`cat clang-tidy-report.txt`
 PAYLOAD_CPPCHECK=`cat cppcheck-report.txt`
 
-echo "Cppcheck errors:"
 echo $PAYLOAD_CPPCHECK
 
 OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
