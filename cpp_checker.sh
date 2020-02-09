d8847f44432441c8f55cdab64445d1c6e5a86bc2
Hassan Pouralijan <pouralijan@gmail.com>
2020-02-09 23:19:39 +0330

    Update cpp_checker.sh

--- cpp_checker.sh
+++ cpp_checker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt src
+echo "$DIRS"
+cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt $DIRS
 
-PAYLOAD_CLANG=`cat clang-tidy-report.txt`
 PAYLOAD_CPPCHECK=`cat cppcheck-report.txt`
 
+echo "Cppcheck errors:"
 echo $PAYLOAD_CPPCHECK
 
 OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
